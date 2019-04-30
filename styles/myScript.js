6e95ff74f52513647ac0dce0bab2a6d2363d1ed2
ivanthleung <50078555+ivanthleung@users.noreply.github.com>
2019-04-30 14:54:33 -0400

    Add files via upload

---
+++ styles/myScript.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    console.log('Good Day Sir!');
+
+
+    // Smooth Scroll
+    $("a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            // Store hash
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    // AOS - Animate on scroll
+    AOS.init();
+
+});
+
+// Nav - Side Slides
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+$( '.overlay-content a' ).on("click", function(){
+    document.getElementById("myNav").style.width = "0%";
+});
